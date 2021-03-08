b43cd451fbec7a3f1e342ab0f920095830ede7af
Favio Silva <cursosdecomputacion.net@gmail.com>
2021-03-08 16:18:35 -0600

    listados

---
+++ lista/lista/Alumno.cs
@@ -0,0 +1,13 @@
+public class Alumno
+{
+    public int id { get; set; }
+    public string Nombre { get; set; }
+
+    public Alumno(int id, string nombre)
+    {
+        id = id;
+        Nombre = nombre;
+    }
+    
+
+}

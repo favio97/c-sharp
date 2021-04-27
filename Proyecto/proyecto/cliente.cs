d944da164e7231fd253850ded55945ffbe82e768
Favio Silva <cursosdecomputacion.net@gmail.com>
2021-04-26 21:59:53 -0600

    Proyecto final

--- Proyecto/proyecto/cliente.cs
+++
@@ -1,9 +0,0 @@
-public class Cliente: Persona
-{
-    public double Saldo { get; set; }
-    public Cliente(int codigo, string nombre)
-    {
-        Codigo = codigo;
-        Nombre = nombre;
-    }
-}
